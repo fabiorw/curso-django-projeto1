978dff32563506a4be5d043ad08cc5ba0fdef867
fabiorw <fabiorwbr@gmail.com>
2023-03-28 10:06:41 -0300

    Aula 62

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
+from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
-
-from .models import Recipe
 
 
 def home(request):
@@ -16,8 +15,14 @@ def home(request):
 def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found :(')
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | '
+
     })
 
 
