1f176a04c536936a755ffa17e45e6427bb544530
fabiorw <fabiorwbr@gmail.com>
2023-03-28 14:56:34 -0300

    Aula 72 testes

--- recipes/tests.py
+++ recipes/tests.py
@@ -3,4 +3,5 @@ from django.test import TestCase
 
 class RecipeURLsTest(TestCase):
     def test_the_pythest_is_ok(self):
-        ...
+        print('Teste um')
+        assert 1 == 1, 'Um é igual a um'
