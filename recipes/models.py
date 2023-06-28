eaa58c623b9b9b86367b9b70098af359cc730c11
fabiorw <fabiorwbr@gmail.com>
2023-06-28 10:43:56 -0300

    Aula 99

--- recipes/models.py
+++ recipes/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
@@ -26,7 +26,9 @@ class Recipe(models.Model):
         upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
 
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None
+        Category, on_delete=models.SET_NULL,
+        null=True, blank=True,
+        default=None
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
