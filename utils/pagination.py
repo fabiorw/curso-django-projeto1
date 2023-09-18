466f8efcdcba7d1ed18f6c009dd50c635e3b4e95
fabiorw <fabiorwbr@gmail.com>
2023-09-18 11:15:36 -0300

    Atualização pagination.py

--- utils/pagination.py
+++ utils/pagination.py
@@ -2,7 +2,7 @@ import math
 
 from django.core.paginator import Paginator
 
-# python -c "import string as s;from random import SystemRandom as sr;print(''.join(sr().choices(s.ascii_letters + s.punctuation, k=64)))"
+# python -c "import string as s;from random import SystemRandom as sr;print(''.join(sr().choices(s.ascii_letters + s.punctuation, k=64)))" # noqa:E501
 
 
 def make_pagination_range(
