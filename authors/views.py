538e6ab648423b351dbe7b73b9742d00a0067883
fabiorw <fabiorwbr@gmail.com>
2024-01-21 11:59:10 -0300

    Aula 150

--- authors/views.py
+++ authors/views.py
@@ -16,11 +16,17 @@ def register_view(request):
 def register_create(request):
     if not request.POST:
         raise Http404()
+
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Your user is created, please log in.')
-        del(request.session['register_form_data'])
+
+        del (request.session['register_form_data'])
+
     return redirect('authors:register')
